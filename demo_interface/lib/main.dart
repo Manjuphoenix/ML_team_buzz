62a8c95ad7b8f638b8c791aa1cde38d815883bbc
Divya Swaroopini Shridhar <swaroopini13@gmail.com>
2022-01-10 17:23:48 +0530

    Implemnted Image Access From cam and gallery

--- demo_interface/lib/main.dart
+++ demo_interface/lib/main.dart
@@ -1,88 +1,125 @@
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+//try wrapping in material app?
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CameraWidgetState());
+  }
+}
+
+class CameraWidgetState extends StatefulWidget {
+  const CameraWidgetState({Key? key}) : super(key: key);
+
   @override
-  _MyPageState createState() => _MyPageState();
+  State<CameraWidgetState> createState() => _CameraWidgetState();
 }
 
-class _MyPageState extends State<MyApp> {
-  /// Variables
-  File imageFile = File(
-      'assets/What-Are-Plants.jpg'); //Add the Path to default image from assets later
+class _CameraWidgetState extends State<CameraWidgetState> {
+  PickedFile? imageFile;
 
-  /// Widget
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  const Divider(
+                    height: 1,
+                    color: Color.fromARGB(128, 128, 128, 10),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      _openGallery(context);
+                    },
+                    title: const Text("Gallery"),
+                    leading: const Icon(
+                      Icons.account_box,
+                      color: Color.fromARGB(128, 128, 128, 10),
+                    ),
+                  ),
+                  const Divider(
+                    height: 1,
+                    color: Color.fromARGB(128, 128, 128, 10),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      _openCamera(context);
+                    },
+                    title: const Text("Camera"),
+                    leading: const Icon(
+                      Icons.camera,
+                      color: Color.fromARGB(128, 128, 128, 10),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("Image Picker"),
+    // Implement build
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Pick Image Camera"),
+        backgroundColor: const Color.fromRGBO(128, 128, 0, 10),
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Card(
+                child: (imageFile == null)
+                    ? const Text("Choose Image")
+                    : Image.file(File(imageFile!.path)),
+              ),
+              MaterialButton(
+                textColor: Colors.white,
+                color: Color.fromRGBO(128, 128, 0, 10),
+                onPressed: () {
+                  _showChoiceDialog(context);
+                },
+                child: const Text("Select Image"),
+              )
+            ],
+          ),
         ),
-        body: Container(
-            child: imageFile == (null)
-                ? Container(
-                    alignment: Alignment.center,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: const Color.fromRGBO(128, 128, 0, 10)),
-                          onPressed: () {
-                            _getFromGallery();
-                          },
-                          child: const Text("PICK FROM GALLERY"),
-                        ),
-                        Container(
-                          height: 40.0,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: const Color.fromARGB(128, 128, 128, 10)),
-                          onPressed: () {
-                            _getFromCamera();
-                          },
-                          child: const Text("PICK FROM CAMERA"),
-                        )
-                      ],
-                    ),
-                  )
-                : Container(
-                    child: Image.file(
-                      imageFile,
-                      fit: BoxFit.cover,
-                    ),
-                  )));
+      ),
+    ));
   }
 
-  /// Get from gallery
-  _getFromGallery() async {
-    final picker = ImagePicker();
-    XFile? pickedFile = await picker.pickImage(
+  void _openGallery(BuildContext context) async {
+    final pickedFile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
-      maxWidth: 1800,
-      maxHeight: 1800,
     );
     setState(() {
-      imageFile = File(pickedFile!.path);
+      imageFile = PickedFile(pickedFile!.path);
     });
+
+    Navigator.pop(context);
   }
 
-  /// Get from Camera
-  _getFromCamera() async {
-    final picker = ImagePicker();
-    XFile? pickedFile = await picker.pickImage(
+  void _openCamera(BuildContext context) async {
+    final pickedFile = await ImagePicker().pickImage(
       source: ImageSource.camera,
-      maxWidth: 1800,
-      maxHeight: 1800,
     );
     setState(() {
-      imageFile = File(pickedFile!.path);
+      imageFile = PickedFile(pickedFile!.path);
     });
+    Navigator.pop(context);
   }
 }
